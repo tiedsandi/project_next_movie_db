d25167b18362c917f115b68c9cd849c8b307d746
tiedsandi <fachransandi@gmail.com>
2024-10-27 19:16:30 +0700

    add home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div className='bg-primary-2'> test</div>;
+  return (
+    <main className='flex flex-col items-center justify-center min-h-screen'>
+      <h1 className='text-4xl font-bold text-center text-white'>Hallo, Folks!</h1>
+      <Image src={'/home-img.png'} alt='hero' width={400} height={400} />
+      <p className='mb-3 text-xl text-center text-white'>Go find your movies</p>
+      <Link href={'/search'} className='px-4 py-2 bg-primary rounded-xl hover:bg-primary-2'>
+        Get Started
+      </Link>
+    </main>
+  );
 }
